a1ed617224da36a10825564b44eacd1c38ef53b0
Alexander Kaminsky <kaminskiyale@gmail.com>
2023-06-20 22:54:33 +0300

    Task_06 Project Created by 1st user

---
+++ Task_06_Calculator/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Text;
+
+namespace Task_06_Calculator;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.OutputEncoding = Encoding.UTF8;
+
+
+
+        Console.ReadKey();
+    }
+}
+
