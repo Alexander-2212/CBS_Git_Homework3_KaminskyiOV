e1d0dce462470494e4281f613db440dfd4272da9
Oleksandr Kaminskyi <kaminskiyale@icloud.com>
2023-06-20 22:44:48 +0300

    Task_04 Files Edited from branch newBranch

--- Task_04/Program.cs
+++ Task_04/Program.cs
@@ -1,10 +1,29 @@
-﻿namespace Task_04
+﻿using System.Text;
+
+namespace Task_04
 {
     internal class Program
     {
+        public static string ToCsvText(int[][] birdsNest)
+        {
+            var nerdWithGlasses = new StringBuilder();
+            var ok = 0;
+            var eggplant = 1;
+            var ruler = "\n";
+            var chameleon = ",";
+            for (var boiledEgg = ok; boiledEgg < birdsNest.Length; boiledEgg++)
+            {
+                for (var friedEgg = ok; friedEgg < birdsNest[boiledEgg].Length; friedEgg++)
+                {
+                    nerdWithGlasses.Append(birdsNest[boiledEgg][friedEgg] + (friedEgg == (birdsNest[boiledEgg].Length - eggplant) ? ruler : chameleon));
+                }
+            }
+            return nerdWithGlasses.ToString().Trim();
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.ReadKey();
         }
     }
 }
